1c40b98ae22cb2f91e02023e3fe8c31f6104eb5e
Ubuntu <sumair.aslam@codingcops.com>
2023-12-15 19:10:03 +0500

    Demo for the founders pass
    This commit contains the running demo for the founders pass functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,174 @@
 import logo from './logo.svg';
 import './App.css';
+import tier1PassImage from './images/founders-pass-silver.png';
+import tier2PassImage from './images/founders-pass-gold.png';
+import { FounderPassAbi, USDTAbi } from './abi/FounderPassAbi';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import React, { useState, useEffect } from 'react';
+import {ethers} from 'ethers';
+import {Card, UserInfo} from './Card';
+import BigNumber from 'bignumber.js';
+BigNumber.config({ EXPONENTIAL_AT: 1e+9 })
 
 function App() {
+  const [userData, setUserData] = useState({
+    userAddress: '',
+    userBalance: '',
+    totalAllowence: '',
+    tier1PassesOwned: '',
+    tier2PassesOwned: ''
+  });
+  const [quantities, setQuantities] = useState([0, 0]);
+  const tier1Price = 250; // Replace with actual price
+  const tier2Price = 4500; // Replace with actual price
+  const foundersPassContractAddress = '0xa3E8f107b1E20a8d33b95ee5D9C9Cd0C6c1f88A1'; // Replace with your smart contract address
+  const usdtContractAddress = '0xe8F3FF106BFb124B1b2572CfC32eD56487949947'
+  const conversionFactor = new BigNumber(10).exponentiatedBy(18);
+
+  // let userAddress, userBalance, totalAllowence, tier1PassesOwned, tier2PassesOwned;
+
+  const populateUserData = async () => {
+    if (window.ethereum) {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      await window.ethereum.enable();
+      
+      const signer = provider.getSigner();
+      const usdtContract = new ethers.Contract(usdtContractAddress, USDTAbi, signer); // Replace with USDT contract details
+      const foundersPassContract = new ethers.Contract(foundersPassContractAddress, FounderPassAbi, signer); // Replace with your smart contract ABI
+
+      const accounts = await provider.listAccounts();
+      const userAddress = accounts[0];
+      const userBalance = await usdtContract.balanceOf(await signer.getAddress());
+      const totalAllowence = await usdtContract.allowance(await signer.getAddress(), foundersPassContractAddress);
+      const tier1PassesOwned = await foundersPassContract.balanceOf(await signer.getAddress(), 1);
+      const tier2PassesOwned = await foundersPassContract.balanceOf(await signer.getAddress(), 2);
+      console.log("userAddress : ", userAddress);
+      console.log("userBalance : ", userBalance.toString());
+      console.log("totalAllowence : ", totalAllowence.toString());
+      console.log("tier1PassesOwned : ", tier1PassesOwned.toString());
+      console.log("tier2PassesOwned : ", tier2PassesOwned.toString());
+      setUserData({
+        userAddress,
+        userBalance: ethers.utils.formatEther(userBalance.toString()),
+        totalAllowence: ethers.utils.formatEther(totalAllowence.toString()),
+        tier1PassesOwned: tier1PassesOwned.toString(),
+        tier2PassesOwned: tier2PassesOwned.toString()
+      });
+    } else {
+      // Metamask not found
+      console.error('Metamask not installed');
+    }
+  }
+  
+
+  const handlePurchaseForTier = async (tier) => {
+    if (window.ethereum) {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      await window.ethereum.enable();
+      
+      const signer = provider.getSigner();
+      const usdtContract = new ethers.Contract(usdtContractAddress, USDTAbi, signer); // Replace with USDT contract details
+      const foundersPassContract = new ethers.Contract(foundersPassContractAddress, FounderPassAbi, signer); // Replace with your smart contract ABI
+
+      try {
+        const amount = ethers.utils.parseUnits(tier == 1? tier1Price.toString(): tier2Price.toString(), 18); // Assuming 18 decimals for USDT
+        const quantity = parseInt(document.getElementById((tier == 1? 'Tier 1 Pass' : 'Tier 2 Pass')).value); 
+        console.log("Quantity : ", quantity)
+        const totalAmount = amount * quantity;        
+        // Send transaction to Metamask wallet
+        let userBalance = await usdtContract.balanceOf(await signer.getAddress());
+        // userBalance = parseFloat(ethers.utils.formatUnits(userBalance)) / Math.pow(10, 18);
+        console.log("User Account : ", await signer.getAddress())
+        console.log("User Balance : ", userBalance.toString());
+        console.log("Amount : ", amount.toString())
+        console.log("type of TotalAmount : ", new BigNumber(totalAmount).toString())
+        console.log("TotalAmount : ", totalAmount.toString())
+        console.log("Balance is greater than total amount? : ", userBalance >= totalAmount)
+        if(userBalance >= totalAmount)
+        {
+          const approvalTx = await usdtContract.approve(foundersPassContractAddress, new BigNumber(totalAmount).toString());
+          toast("Approval Pending");
+          const receipt = await approvalTx.wait();
+          if (receipt && receipt.blockNumber) {
+            const transaction = await (tier == 1 ? foundersPassContract.buyTier1Pass(quantity): foundersPassContract.buyTier2Pass(quantity)); // Call your smart contract function here
+            toast("Minting Passes");
+            console.log('Transaction hash:', transaction.hash);
+          }
+          else {
+            alert("Approval failed");
+          }
+        }
+        else
+          alert("Insuffcient Balance")
+        
+        // Send transaction to your smart contract
+        
+        // Handle success
+      } catch (error) {
+        // Handle error
+        console.error(error);
+      }
+    } else {
+      // Metamask not found
+      console.error('Metamask not installed');
+    }
+  };
+
+  // const handlePurchaseForTier2 = async () => {
+  //   if (window.ethereum) {
+  //     const provider = new ethers.providers.Web3Provider(window.ethereum);
+  //     await window.ethereum.enable();
+      
+  //     const signer = provider.getSigner();
+  //     const usdtContract = new ethers.Contract(usdtContractAddress, USDTAbi, signer); // Replace with USDT contract details
+  //     const foundersPassContract = new ethers.Contract(foundersPassContractAddress, FounderPassAbi, signer); // Replace with your smart contract ABI
+
+  //     try {
+  //       const amount = ethers.utils.parseUnits(tier2Price.toString(), 18); // Assuming 18 decimals for USDT
+  //       const quantity = parseInt(document.getElementById('quantity').value); 
+
+  //       const totalAmount = amount.mul(quantity);
+  //       let userBalance = await usdtContract.balanceOf(await signer.getAddress());
+  //       console.log("User Account : ", await signer.getAddress())
+  //       console.log("User Balance : ", userBalance.toString());
+  //       console.log("Amount : ", amount.toString())
+  //       // Send transaction to Metamask wallet
+        
+  //       if(userBalance >= totalAmount)
+  //         await usdtContract.approve(foundersPassContractAddress, totalAmount.toString());
+  //       else
+  //         alert("Insuffcient Balance")
+        
+  //       // Send transaction to your smart contract
+  //       const transaction = await foundersPassContract.buyTier2Pass(quantity); // Call your smart contract function here
+
+  //       console.log('Transaction hash:', transaction.hash);
+  //       // Handle success
+  //     } catch (error) {
+  //       // Handle error
+  //       console.error(error);
+  //     }
+  //   } else {
+  //     // Metamask not found
+  //     console.error('Metamask not installed');
+  //   }
+  // };
+
+  useEffect(() => {
+    populateUserData();
+ }, [userData]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 class="sal-heading">Items for Sale</h1>
+      <div>
+        <ToastContainer />
+      </div>
+      <UserInfo walletAddress={userData.userAddress} totalAllowence={userData.totalAllowence} totalBalance={userData.userBalance} tier1PassesOwned={userData.tier1PassesOwned} tier2PassesOwned={userData.tier2PassesOwned}/>
+      <Card itemName="Tier 1 Pass" price={tier1Price} imageUrl={"https://assets.playgroundai.com/27866d57-e6f8-471e-9acc-eb77aa669c97.jpg"} handlePurchase={event => handlePurchaseForTier(1)} quantity={quantities[0]} setQuantity={(quantity) => setQuantities([quantity, quantities[1]])}/>
+      <Card itemName="Tier 2 Pass" price={tier2Price} imageUrl={"https://assets.playgroundai.com/16d0a622-bc92-4a38-aaa2-1e9787acaa3b.jpg"} handlePurchase={event => handlePurchaseForTier(2)} quantity={quantities[1]} setQuantity={(quantity) => setQuantities([quantities[0], quantity])}/>
     </div>
   );
 }
---
+++ src/Card.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import './Card.css';
+
+export const Card = ({ itemName, price, imageUrl, handlePurchase, quantity, setQuantity }) => {
+  const validateQuantity = (value) => {
+    if (value > 100) {
+      alert('Quantity should not exceed 100.');
+      return quantity;
+    }
+    return value;
+ };
+  return (
+    <div className="card-container">
+      <div className="card">
+      <img src={imageUrl} alt={itemName} className="card-image" width="300" height="300" position='top'/>
+        <h2>{itemName}</h2>
+        <p>Price: {price} USDT</p>
+        <div className="card-input">
+          <label htmlFor="quantity">Quantity:</label>
+          <input
+            type="number"
+            id={itemName}
+            name="quantity"
+            value={quantity}
+            onChange={(e) => setQuantity(validateQuantity(parseInt(e.target.value)))}
+          />
+        </div>
+        <button onClick={handlePurchase}>Purchase</button>
+      </div>
+    </div>
+  );
+};
+
+export const UserInfo = ({ walletAddress, totalAllowence, totalBalance, tier1PassesOwned, tier2PassesOwned }) => {
+  return (
+    <div className="card-container">
+      <div className="card">
+        <h2>User Info</h2>
+        <p>Wallet Address: {walletAddress}</p>
+        <p>Balance: {totalBalance} USDT</p>
+        <p>Total Approved Allownece : {totalAllowence} USDT</p>
+        <p>Tier 1 Owned Passes: {tier1PassesOwned}</p>
+        <p>Tier 2 Owned Passes: {tier2PassesOwned}</p>
+      </div>
+    </div>
+  );
+};
